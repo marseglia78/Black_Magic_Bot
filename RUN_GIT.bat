7b85a655983405fea257695939d6b7698bd28f2c
Marseglia <lumarseglia@deloitte.com>
2018-09-20 15:13:17 -0700

    commit

--- RUN_GIT.bat
+++ RUN_GIT.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "First commit"
-git remote add origin https://github.com/marseglia78/Black_Magic_Bot.git
+git commit -m "commit"
+#git remote add origin https://github.com/marseglia78/Black_Magic_Bot.git
 git remote -v
 git push origin master
