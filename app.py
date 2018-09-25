e4d1d7c267792f5a129bbb34883e14788919eecf
Marseglia <lumarseglia@deloitte.com>
2018-09-25 15:44:45 -0700

    commit

--- app.py
+++ app.py
@@ -14,27 +14,20 @@ VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 #ACCESS_TOKEN='EAAd0fYx51rcBANxZB9zdKQIYxuR3GEPqK60TAefCJ7TiZCsF14pYz4kcaSQgflqum3p5qhdv9qMG2XZAxxec8fCbJ3i7wWq1CbqWBqkz3H2u7cXcUTqtvSZAIdzkFqmAkogiefCgZCAEPxxcqaUkr7hZCZCtGrPOAZCYlCkZCi6vvHgZDZD'
 bot = Bot(ACCESS_TOKEN)
 
-#'''
+'''
 @app.route("/", methods=['GET', 'POST'])
-#@app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
-        """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook."""
+        
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
-        '''
-        data = request.json
-        sender_id = data['entry'][0]['messaging'][0]['sender']['id']
-        send_message(sender_id, "your app is succesfully deployed")
-        return "Message Processed"
-        '''
+        
     else:
         data = request.json
         sender_id = data['entry'][0]['messaging'][0]['sender']['id']
         send_message(sender_id, "your app is succesfully deployed")
         return "Message Processed"
-#'''
+'''
 
 ##We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/webhook", methods=['GET', 'POST'])
